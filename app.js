b975c73aefa043822080f2bf1cc4f54ccf269f6c
Abhishek <abhishek.p.3.14@gmail.com>
2019-02-24 23:21:24 -0500

    Base files for blog development

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+// public static data
+app.use(express.static('public'));
+
+// view engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// public functions
+app.get('/', function(req, res) {
+  res.render('index', { Title: 'Abhishek\'s Blog', 
+  Message: 'First Post',
+});
+
+// port listening on 
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
